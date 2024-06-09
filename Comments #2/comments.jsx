b28ddfa2aa8f8a8cc69ff54316c768b958803805
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2024-06-10 02:58:26 +0530

    Baisc Part 1 Added

---
+++ Comments #2/comments.jsx
@@ -0,0 +1,12 @@
+// How to comment out JSX elements
+
+{
+  /*
+const header = (
+    <header>
+    <h1>I am a developer</h1>
+    <h2> Hello World! </h2>
+    </header>
+    )
+    */
+}
---
+++ Intro Code #1/index.jsx
@@ -0,0 +1,14 @@
+// Don't add quotes with JSX
+const jsxElement = <h1>I am a developer</h1>;
+const data = <h2>20 Sep</h2>; // It's JavaScript with HTML
+
+// Adding content to the JSX Element by adding HTML Elements
+
+const header = (
+<header>
+<h1>I am a developer</h1>
+<h2> Hello World! </h2>
+</header>
+)
+
+
